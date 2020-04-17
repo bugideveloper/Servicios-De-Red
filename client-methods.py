5028110bba8e061de848d5318113f75df3389f42
zuturito <zuturito@hotmail.com>
2020-04-17 13:04:09 -0500

    Update client-methods.py

--- client-methods.py
+++ client-methods.py
@@ -1,5 +1,5 @@
 import os
-
+#método para listar archivos
 def get_archivos(path):
     print("Obteniendo archivos del directorio local")
     path = path
@@ -8,5 +8,16 @@ def get_archivos(path):
     for archivo in archivos:
         print(archivo)
     print("[Directorios Mostrados]")
-get_archivos(".")
 #print(os.name)
+
+#método para obtener el detalle de un archivo seleccionado
+def get_detalle(path, archivo):
+	print("[Detalle del Archivo]")
+	print('Nombre del archivo         :', archivo)
+	print('Último acceso  :', time.ctime(os.path.getatime(archivo)))
+	print('Fecha de Modificación:', time.ctime(os.path.getmtime(archivo)))
+	print('Tamaño         :', os.path.getsize(archivo))
+
+#llamadas a los métodos
+get_archivos(".")
+get_detalle(".","paisaje-1.jpg")
