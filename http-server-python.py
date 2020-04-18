11e71ab4997bd96d8750ceb37270ae83e30e2879
bugideveloper <54685679+bugideveloper@users.noreply.github.com>
2020-04-18 15:53:02 -0500

    Update Commit
    Comments Deleted

--- http-server-python.py
+++ http-server-python.py
@@ -1,12 +1,14 @@
-import http.server
-import socketserver
 
 #[Referencias]
 #Faltan Metodo Get y Post
 #https://blog.anvileight.com/posts/simple-python-http-server/
 #https://www.afternerd.com/blog/python-http-server/
 
-PORT = 8080
+import http.server
+import socketserver
+import socket,pickle
+
+PORT = 8081
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
