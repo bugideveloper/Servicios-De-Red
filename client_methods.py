b96cb4ecdd2b2fe009adf026a1227d83ae358a9b
bugideveloper <54685679+bugideveloper@users.noreply.github.com>
2020-04-17 19:24:40 -0500

    File commit
    Commit de los archivos

--- client_methods.py
+++ client_methods.py
@@ -1,13 +1,11 @@
 import os
+import time
 #método para listar archivos
 def get_archivos(path):
     print("Obteniendo archivos del directorio local")
     path = path
     archivos = os.listdir(path)
-    print("[Imprimiendo archivos]")
-    for archivo in archivos:
-        print(archivo)
-    print("[Directorios Mostrados]")
+    return archivos
 #print(os.name)
 
 #método para obtener el detalle de un archivo seleccionado
@@ -18,6 +16,7 @@ def get_detalle(path, archivo):
 	print('Fecha de Modificación:', time.ctime(os.path.getmtime(archivo)))
 	print('Tamaño         :', os.path.getsize(archivo))
 
-#llamadas a los métodos
-get_archivos(".")
-get_detalle(".","paisaje-1.jpg")
+#[Lista de archivos]
+#archivos = get_archivos(".")
+#for archivo in archivos:
+#    print(archivo)
